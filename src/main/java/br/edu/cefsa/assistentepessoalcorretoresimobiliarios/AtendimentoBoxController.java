88c629137556016ee13d3d09f4ecb1597272e27e
VINICIUS AFONSO DOS SANTOS <082200006@faculdade.cefsa.edu.br>
2023-05-15 22:26:24 -0300

    Ok

--- src/main/java/br/edu/cefsa/assistentepessoalcorretoresimobiliarios/AtendimentoBoxController.java
+++ src/main/java/br/edu/cefsa/assistentepessoalcorretoresimobiliarios/AtendimentoBoxController.java
@@ -57,7 +57,6 @@ public class AtendimentoBoxController {
 
         try {
             ClienteDAO dao = new ClienteDAO();
-            
             cbCliente.setConverter(new StringConverter<Cliente>() {
                 @Override
                 public String toString(Cliente object) {
@@ -73,12 +72,13 @@ public class AtendimentoBoxController {
                     return null;
                 }
             });
+
             List<Cliente> listaClientes = dao.listar();
             ObservableList ObList = FXCollections.observableList(listaClientes);
             cbCliente.setItems(ObList);
             ClienteDAO clientedao = new ClienteDAO();
             Cliente cliente = new Cliente(atendimento.getClienteID());
-            cbCliente.setValue(clientedao.listarPorID(cliente));
+            cbCliente.getSelectionModel().select(clientedao.listarPorID(cliente));
             txtAnotacao.setText(atendimento.getAnotacoes());
             atendimentoTitulo.setText("Atendimento #");
             txtAnotacao.setDisable(true);
@@ -102,12 +102,11 @@ public class AtendimentoBoxController {
         try {
             atendimentoTitulo.setText("Atendimento ##");
             ClienteDAO dao = new ClienteDAO();
-            
-            
+
             cbCliente.setConverter(new StringConverter<Cliente>() {
                 @Override
                 public String toString(Cliente object) {
-                    
+
                     if (object == null) {
                         return "";
                     } else {
